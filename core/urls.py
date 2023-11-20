eec7ef5de0d498b4b3ed480d2b0a64e867159e74
Hk669 <hrushi669@gmail.com>
2023-11-20 23:48:44 +0530

    profile page

--- core/urls.py
+++ core/urls.py
@@ -11,4 +11,4 @@ urlpatterns = [
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/models.py
+++ main/models.py
@@ -7,9 +7,16 @@ class User(AbstractUser):
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True, blank=True)
     hackathon_participant = models.BooleanField(default=True, null=True)
+    avatar = models.ImageField(default='default.png')
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
+
+    #socials
+    twitter = models.URLField(max_length=500, null=True, blank=True)
+    linkedin = models.URLField(max_length=500, null=True, blank=True)
+    website = models.URLField(max_length=500, null=True, blank=True)
+    github = models.URLField(max_length=500, null=True, blank=True)
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -22,7 +29,9 @@ class Event(models.Model):
     title = models.CharField(max_length=200, null=False)
     description = models.TextField(null=False, blank=False)
     participants = models.ManyToManyField(User, blank=True, related_name='events')
-    date = models.DateField()
+    start_date = models.DateField(null=True)
+    end_date = models.DateField(null=True)
+    reg_deadline = models.DateTimeField(null=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
