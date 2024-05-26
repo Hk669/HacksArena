a5efa5aa29e3ebebfadbcd4d0df46b4ef593a83e
Hk669 <hrushi669@gmail.com>
2024-05-26 16:29:17 +0530

    [fix] redirect uris

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('main.urls')),
-    path('accounts/',include('allauth.urls')),
+    # path('accounts/',include('allauth.urls')),
     path('accounts/', include('allauth.socialaccount.urls'), name='social'),
 ]
 
